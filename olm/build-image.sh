58af4d17f87a6c1f22466417038e5b21cdfb2ce1
Prasad Shirodkar <prasad.shirodkar@oracle.com>
2025-04-15 05:52:04 -0700

    set golang

---
+++ olm/build-image.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -x
+
+CONTAINER_CLI="${CONTAINER_CLI:-podman}"
+
+name="csi-snapshotter"
+version="{{{ .major }}}.{{{ .minor }}}.{{{ .patch }}}"
+registry="container-registry.oracle.com/olcne"
+docker_tag=${registry}/${name}:v${version}
+
+"${CONTAINER_CLI}" build --pull \
+    --build-arg https_proxy=${https_proxy} \
+    --volume /etc/yum.repos.d:/etc/yum.repos.d \
+    --tag ${docker_tag} -f ./olm/builds/Dockerfile_csiSnapshotter .
+
+name="snapshot-controller"
+docker_tag=${registry}/${name}:v${version}
+
+"${CONTAINER_CLI}" build --pull \
+    --build-arg https_proxy=${https_proxy} \
+    --volume /etc/yum.repos.d:/etc/yum.repos.d \
+    --tag ${docker_tag} -f ./olm/builds/Dockerfile_snapshotController .
